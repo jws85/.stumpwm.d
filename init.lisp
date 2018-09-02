1c665f78f4b6ff45fb53e84b6d60492df0b48bc5
J. W. Smith <jwsmith2spam at gmail dot com>
2018-09-02 16:01:02 -0400

    Add keybinding for terminal

--- init.lisp
+++ init.lisp
@@ -59,9 +59,7 @@
 
 ;; Applications
 (define-key *top-map* (kbd "s-SPC") "exec rofi -show run -display-run \">>> \"")
-;(define-key *root-map* (kbd "F1") "exec firefox")
-;(define-key *root-map* (kbd "F2") "exec emacs")
-;(define-key *root-map* (kbd "F3") "exec sakura")
+(define-key *top-map* (kbd "s-RET") "exec sakura")
 
 ;; Modeline ------------------------------------------------------------------
 
